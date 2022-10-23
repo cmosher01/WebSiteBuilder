c0ace976f7362f469f3b781fcb16ca2e1ca0ebad
Christopher A. Mosher <cmosher01@gmail.com>
2022-10-23 18:29:28 -0400

    fix build

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@
 date
 whoami
 pwd
-ls -al
 git --version
 java -version
 
@@ -12,19 +11,12 @@ git config --global user.name "build.sh"
 
 cid=$(git rev-parse HEAD)
 
-git show-ref
-git branch -avv
-git status
-ls -l ./.git/refs/heads/
-
 git checkout docs
 git rebase main
 mkdir -p docs
-ls -l docs
 ./gradlew run -q >docs/index.html
-ls -l docs
 git add docs
 git status
 git diff --cached
 git commit -m "auto build of $cid"
-git push
+git push -f
